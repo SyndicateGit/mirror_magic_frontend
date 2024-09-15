4bdc42a02265a3f845a7dd4196381889c08c1489
Raymond Zeng <shilei.zeng98@outlook.com>
2024-09-15 01:13:17 -0400

    results page wackity wack wack

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -2,6 +2,9 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import drip from "../../../public/assets/vector.png";
+import wave from "../../../public/assets/Component9.png";
+import mirror from "../../../public/assets/m_m.png";
+import crown from "../../../public/assets/crown.png";
 import {
   Carousel,
   CarouselContent,
@@ -13,7 +16,8 @@ import { Card, CardContent } from '@/components/ui/card';
 
 const Page = () => {
   // State to store result from localStorage
-  const [result, setResult] = useState({});
+  const [result, setResult] = useState<{shopify: string, image: string}>({});
+
 
   // Effect to retrieve data from localStorage once the component has mounted
   useEffect(() => {
@@ -23,48 +27,24 @@ const Page = () => {
       if (storedResult) {
         setResult(JSON.parse(storedResult));
       }
+      setResult({shopify: "https://www.shopify.com", image: "https://www.cloudinary.com"});
     }
   }, []);
 
   console.log('Result:', result);
 
   return (
-    <div className='w-full bg-wet'>
-      <section className='flex flex-col gap-6 items-center w-full h-[1000px]'>
-        <Image src={drip} width={2047} height={600} alt={""} className="absolute z-[-1]" />
-        {/* Display result (optional) */}
-        <div>
-          {result ? (
-            <pre>{JSON.stringify(result, null, 2)}</pre>
-          ) : (
-            <p>No result found in localStorage</p>
-          )}
+    <div className='w-full h-full flex flex-col justify-center items-center'>
+        <Image src={drip} width={2047} height={1200} alt={""} className="absolute top-0" />
+        <Image src={mirror} width={75} height={75} alt={"mirror"} className="absolute top-2 left-2" />
+      <section className='flex mt-[100px] justify-around items-center w-full h-full'>
+        <div className='flex flex-col justify-center items-center'>
+          <Image src={crown} width={600} height={400} alt=''/>
+          <img src={result.image} alt="User Image" width={200} height={200} className="rounded-lg" />
         </div>
-          <div className='mt-[100px]'>
-          <Carousel
-            opts={{
-              align: "start",
-            }}
-            className="w-full max-w-sm"
-          >
-            <CarouselContent>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                  <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-video items-center justify-center p-6">
-                        <span className="text-3xl font-semibold">{index + 1}</span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-          </div>
+        <input className='w-64 h-12' type="text" placeholder={result.shopify} />
       </section>
+      <button className='self-center bg-[#7E958F] text-white font-bold px-24 pt-10 rounded-sm flex justify-center items-center'>Confirm</button>        
     </div>
   );
 };
